035102a875dde26813626cad77fa02cf69e760a4
Traikocik <wanilia12@interia.pl>
2024-12-11 19:59:55 +0100

    TEST

---
+++ Views/ProductView.xaml.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Linq;
+using Microsoft.Maui.Controls;
+
+namespace ShoppingList4F1.Views;
+
+public partial class ProductView : ContentView
+{
+    public ProductView()
+    {
+        InitializeComponent();
+    }
+
+    private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender is CheckBox checkBox && checkBox.BindingContext is Models.Product changedProduct)
+        {
+            var shoppingList = (Models.ShoppingList)BindingContext;
+            var currentProduct = shoppingList.Products.FirstOrDefault(p => p.Id == changedProduct.Id);
+            if (currentProduct != null)
+            {
+                currentProduct.IsBought = e.Value;
+                Models.AllShoppingLists.SaveShoppingLists();
+            }
+        }
+    }
+
+    private void IncrementButton_Clicked(object sender, EventArgs e)
+    {
+        if (sender is ImageButton imageButton && imageButton.BindingContext is Models.Product changedProduct)
+        {
+            var shoppingList = (Models.ShoppingList)BindingContext;
+            var currentProduct = shoppingList.Products.FirstOrDefault(p => p.Id == changedProduct.Id);
+            if (currentProduct != null)
+            {
+                currentProduct.Quantity++;
+                Models.AllShoppingLists.SaveShoppingLists();
+            }
+        }
+    }
+
+    private void DecrementButton_Clicked(object sender, EventArgs e)
+    {
+        if (sender is ImageButton imageButton && imageButton.BindingContext is Models.Product changedProduct)
+        {
+            var shoppingList = (Models.ShoppingList)BindingContext;
+            var currentProduct = shoppingList.Products.FirstOrDefault(p => p.Id == changedProduct.Id);
+            if (currentProduct != null)
+            {
+                currentProduct.Quantity--;
+                Models.AllShoppingLists.SaveShoppingLists();
+            }
+        }
+    }
+
+    private void RemoveButton_Clicked(object sender, EventArgs e)
+    {
+        if (sender is ImageButton imageButton && imageButton.BindingContext is Models.Product productToBeDeleted)
+        {
+            var shoppingList = (Models.ShoppingList)BindingContext;
+            shoppingList.Products.Remove(productToBeDeleted);
+            Models.AllShoppingLists.SaveShoppingLists();
+        }
+    }
+}
