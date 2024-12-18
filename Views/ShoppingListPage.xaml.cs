4cf5568b8264148747886be54afcb499fcc029ce
Traikocik <wanilia12@interia.pl>
2024-12-18 20:14:54 +0100

    Fix: Categories without products are no longer shown

--- Views/ShoppingListPage.xaml.cs
+++ Views/ShoppingListPage.xaml.cs
@@ -44,6 +44,14 @@ public partial class ShoppingListPage : ContentPage
         });
     }
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        SetCollectionToCategoriesWithProducts();
+    }
+
+    
+
     protected override void OnDisappearing()
     {
         base.OnDisappearing();
@@ -73,13 +81,13 @@ public partial class ShoppingListPage : ContentPage
         categoriesCollection.ItemsSource = categoriesWithNotBoughtProduct;
         IsFilteringByNotBought = true;
         resetButton.IsEnabled = true;
-        
+
     }
 
     private async void FilterByShop_Clicked(object sender, EventArgs e)
     {
         string[] shopNames = Models.AllShoppingLists.Shops.Select(shop => shop.Name).ToArray();
-        string[] newShopNames = new string[shopNames.Length+1];
+        string[] newShopNames = new string[shopNames.Length + 1];
         newShopNames[0] = "-";
         Array.Copy(shopNames, 0, newShopNames, 1, shopNames.Length);
 
@@ -87,7 +95,7 @@ public partial class ShoppingListPage : ContentPage
 
         if (!string.IsNullOrEmpty(selectedShopName) && selectedShopName != "Cancel")
         {
-            Models.Shop selectedShop = selectedShopName != "-" ? 
+            Models.Shop selectedShop = selectedShopName != "-" ?
                 Models.AllShoppingLists.Shops.FirstOrDefault(shop => shop.Name == selectedShopName) :
                 new Models.Shop("", "");
             if (selectedShop != null)
@@ -106,12 +114,12 @@ public partial class ShoppingListPage : ContentPage
             }
         }
     }
-    
+
     private void ResetFilterButton_Clicked(object sender, EventArgs e)
     {
-        categoriesCollection.ItemsSource = ((Models.ShoppingList)BindingContext).Categories;
         resetButton.IsEnabled = false;
         IsFilteringByNotBought = false;
+        SetCollectionToCategoriesWithProducts();
     }
 
     private async void ExportShoppingList_Clicked(object sender, EventArgs e)
@@ -153,6 +161,19 @@ public partial class ShoppingListPage : ContentPage
                 category.IsExpanded = !category.IsExpanded;
             }
         }
+    }
+
+    private void SetCollectionToCategoriesWithProducts()
+    {
+        List<Models.Category> categories = ((Models.ShoppingList)BindingContext).Categories
+            .Select(category => new Models.Category(category.Id, category.Name)
+            {
+                Products = new ObservableCollection<Models.Product>(category.Products.Where(product => product.ShopId == product.ShopId))
+            })
+            .Where(category => category.Products.Any())
+            .ToList();
+
+        categoriesCollection.ItemsSource = categories;
     }
 
     //private void CategoryView_CategoryChanged(object sender, EventArgs e)
